0bf871d235d51bb046eb67dec19c68fa62ad4f55
Vladimir Goshev <vgoshev@yandex-team.ru>
2025-09-20 17:28:41 +0300

    Better run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,18 @@ cd `dirname $0`
 . ./.env
 
 [ -z "$ENV" ] && ENV="local"
+[ -z "$COMPRESSOR"] && COMPRESSOR="xz -9e"
+[ -z "$DO_PIP_UPDATE"] && DO_PIP_UPDATE="1"
 
 mkdir -p logs
+# Compress old logs
+for log in logs/*.log; do
+    $COMPRESSOR $log
+done
+
+# Do PIP Update if needed
+if [ "$DO_PIP_UPDATE" = "1" ]; then
+    ./venv/bin/pip install -r ./requirements.txt
+fi
+
 ./venv/bin/python ./main.py --config-dir ./configs/00-defaults --config-dir "./configs/$ENV" $* 2>&1 | tee `date '+logs/%Y-%m-%d_%H-%M.log'`
