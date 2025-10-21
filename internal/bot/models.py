5bf38fec8373b63b0f5f2b58ca062e52f0871a5f
Vladimir Goshev <git@sunx.cc>
2025-10-21 18:26:56 +0300

    Basic handling of all media types

--- internal/bot/models.py
+++ internal/bot/models.py
@@ -25,11 +25,23 @@ class LLMMessageFormat(StrEnum):
 
 class MessageType(StrEnum):
     TEXT = "text"
+    # PHOTO      - https://docs.python-telegram-bot.org/en/stable/telegram.photosize.html#telegram.PhotoSize
     IMAGE = "image"
-    # VIDEO = "video"
-    # AUDIO = "audio"
-    # DOCUMENT = "document"
+    # STICKER    - https://docs.python-telegram-bot.org/en/stable/telegram.sticker.html#telegram.Sticker
     STICKER = "sticker"
+    # ANIMATION  - https://docs.python-telegram-bot.org/en/stable/telegram.animation.html#telegram.Animation
+    ANIMATION = "animation"
+    # VIDEO      - https://docs.python-telegram-bot.org/en/stable/telegram.video.html#telegram.Video
+    VIDEO = "video"
+    # VideoNote  - https://docs.python-telegram-bot.org/en/stable/telegram.videonote.html#telegram.VideoNote
+    VIDEO_NOTE = "video-note"
+    # AUDIO      - https://docs.python-telegram-bot.org/en/stable/telegram.audio.html#telegram.Audio
+    AUDIO = "audio"
+    # VOICE      - https://docs.python-telegram-bot.org/en/stable/telegram.voice.html#telegram.Voice
+    VOICE = "voice"
+    # DOCUMENT   - https://docs.python-telegram-bot.org/en/stable/telegram.document.html#telegram.Document
+    DOCUMENT = "document"
+    # CHAT_PHOTO - https://docs.python-telegram-bot.org/en/stable/telegram.chatphoto.html#telegram.ChatPhoto
     UNKNOWN = "unknown"
 
 
