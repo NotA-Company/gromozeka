7292155703936babe6ef17232542b487fe226066
Vladimir Goshev <git@sunx.cc>
2025-11-02 04:00:13 +0300

    Add Dolden Tests for YandexSearch API Client

---
+++ tests/golden_data/yandex_search/__init__.py
@@ -0,0 +1,25 @@
+"""
+Golden Data Provider for YandexSearchClient testing.
+
+This module provides a GoldenDataProvider class that can patch the YandexSearchClient
+to use golden data instead of making real API calls during testing.
+"""
+from .provider import GoldenDataProvider
+
+# Export types
+from .types import (
+    YandexSearchCallInfo,
+    YandexSearchGoldenDataEntry,
+    YandexSearchGoldenDataFile,
+    YandexSearchRequestInfo,
+    YandexSearchResponseInfo,
+)
+
+__all__ = [
+    "GoldenDataProvider",
+    "YandexSearchCallInfo",
+    "YandexSearchGoldenDataEntry",
+    "YandexSearchGoldenDataFile",
+    "YandexSearchRequestInfo",
+    "YandexSearchResponseInfo",
+]
