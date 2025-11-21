8a08543d3152a0e78949581970f61539b00488b3
Vladimir Goshev <vgoshev@yandex-team.ru>
2025-11-21 03:27:23 +0300

    Fixed docstrings in internal

--- internal/bot/common/models/wrappers.py
+++ internal/bot/common/models/wrappers.py
@@ -1,4 +1,8 @@
-"""TODO"""
+"""Type wrappers for multi-platform bot update objects.
+
+Provides unified type definitions for handling update objects from different
+bot platforms (Telegram and Max Messenger) in the common bot architecture.
+"""
 
 from typing import Union
 
