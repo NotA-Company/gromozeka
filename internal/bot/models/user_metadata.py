b345d852241db2c97c929a4181eabf12c4af86f2
Vladimir Goshev <git@sunx.cc>
2026-01-08 00:56:59 +0300

    Add ability to drop all messages from some user

--- internal/bot/models/user_metadata.py
+++ internal/bot/models/user_metadata.py
@@ -3,3 +3,4 @@ from typing import NotRequired, TypedDict
 
 class UserMetadataDict(TypedDict):
     notSpammer: NotRequired[bool]  # True if user defined as not spammer
+    dropMessages: NotRequired[bool]  # True if bot need to delete all new user messages
