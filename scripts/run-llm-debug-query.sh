1fd0a4626900fbe5c6c2416f2ed5a01ed10bef2f
Vladimir Goshev <git@sunx.cc>
2026-05-10 10:58:01 +0300

    Add LLM-debug scripts

---
+++ scripts/run-llm-debug-query.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#set -x
+
+cd "`dirname $0`/.."
+ENV_FILE=".env"
+
+case "$1" in
+    --env=*)
+        env_value=`echo "$1" | cut -d= -f2`
+        ENV_FILE="$ENV_FILE.$env_value"
+        shift
+        ;;
+esac
+
+# Do not show env file content as it can content secrets
+set +x
+. "$ENV_FILE"
+#set -x
+
+[ -z "$CONFIGS" ] && CONFIGS="local"
+
+CONFIG_DIRS=""
+for v in $CONFIGS; do
+    CONFIG_DIRS="$CONFIG_DIRS --config-dir ./configs/$v"
+done
+
+./venv/bin/python scripts/run_llm_debug_query.py --dotenv-file "$ENV_FILE" --config-dir ./configs/00-defaults $CONFIG_DIRS $*
